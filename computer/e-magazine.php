54cfd759709d570d860b79b45863c03e91cabd93
Rajat Hemant Deore <rajat.72002@gmail.com>
2022-10-31 09:54:47 +0530

    Day 7

---
+++ computer/e-magazine.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<title>Download - E-magazine of Department of Computer Engineering | Pimpri Chinchwad College of Engineering And Research | PCCOER - PCMC, Pune</title>
+	<meta name="description" content="Download the  e-magazine of department of, Computer Engineering Pimpri Chinchwad College of Engineering And Research." />
+	<meta name="keywords" content="pimpri chinchwad college of engineering and research, computer engineering, e-magazine">
+
+	<!-- Bootstrap -->
+	<link href="../css/bootstrap.min.css" rel="stylesheet">
+	<link href="../css/style.css" rel="stylesheet">
+	<link href="../css/pccoer.css" rel="stylesheet">
+	<link href="../css/menu.css" rel="stylesheet">
+	<link rel="icon" href="../images/pccoer-favicon-icon.png" type="image/png">
+
+	<!--Font Awesome Stylesheets -->
+	<link href="../css/all.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+	<?php include '../header.php'; ?>
+
+	<div class="inner-page-banner"></div>
+
+	<div class="page_breadcrumb">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<nav aria-label="breadcrumb">
+						<ol class="breadcrumb">
+							<li class="breadcrumb-item"><i class="fa fa-home"></i> Home</li>
+							<li class="breadcrumb-item">Computer</li>
+							<li class="breadcrumb-item"><a href="#">E-Magazine</a></li>
+						</ol>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="page_details">
+		<div class="container">
+			<div class="row">
+
+				<?php include 'computer-sidebar.php'; ?>
+
+				<div class="col-xs-12 col-sm-12 col-md-9">
+					<div class="hdg-bdr"><span class="hdg-bdr-1"></span><span class="hdg-bdr-2"></span><span class="hdg-bdr-3"></span></div>
+					<br>
+					<h1 align="center">E-Magazine</h1>
+					<hr>
+
+					<div class="col-md-12">
+
+						<div class="col-md-3">
+							<a href="pdf/download21/Academic-Calendar-AY-2021-22-TERM-II.pdf" target="_blank"><img src="./../applied-science/images/download.png" alt="Research Publications"></a>
+							<p>2021-22 Sem 1</p>
+						</div>
+
+						<div class="col-md-3">
+							<a href="pdf/download21/SE%20Computer%20Engg.%202019%20Patt_03.072020.pdf" target="_blank"><img src="./../applied-science/images/download.png" alt="Research Publications"></a>
+							<p>2020-21 Sem1</p>
+						</div>
+
+						<div class="col-md-3">
+							<a href="pdf/download21/SPPU_BE_Computer_2015Course_Syllabus_11th%20June2018.pdf" target="_blank"><img src="./../applied-science/images/download.png" alt="Research Publications"></a>
+							<p>2020-21 Sem2</p>
+						</div>
+
+						<div class="col-md-3">
+							<a href="pdf/download21/SPPU_SE_Computer_Engg_2015_Course_Syllabus-4-7-16.pdf" target="_blank"><img src="./../applied-science/images/download.png" alt="Research Publications"></a>
+							<p>2019-20 Sem1</p>
+						</div>
+
+						<div class="col-md-3">
+							<a href="pdf/download21/project_list_2017-18.pdf" target="_blank"><img src="./../applied-science/images/download.png" alt="Research Publications"></a>
+							<p>2019-20 Sem2</p>
+						</div>
+						<div class="col-md-3">
+							<a href="pdf/download21/project_list_2018-19.pdf" target="_blank"><img src="./../applied-science/images/download.png" alt="Research Publications"></a>
+							<p>2018-19 Sem1</p>
+						</div>
+
+						<div class="col-md-3">
+							<a href="pdf/download21/project_list_2019-20.pdf" target="_blank"><img src="./../applied-science/images/download.png" alt="Research Publications"></a>
+							<p>2018-19 Sem2</p>
+						</div>
+
+						<div class="col-md-3">
+							<a href="pdf/download21/project_list_2020-21.pdf" target="_blank"><img src="./../applied-science/images/download.png" alt="Research Publications"></a>
+							<p>2017-18 Sem1</p>
+						</div>
+
+						<div class="col-md-3">
+							<a href="pdf/download21/Syllabus%20TE%20Computer%20Engineering%202012Course.pdf" target="_blank"><img src="./../applied-science/images/download.png" alt="Research Publications"></a>
+							<p>2017-18 Sem2</p>
+						</div>
+
+						<div class="col-md-3">
+							<a href="pdf/download21/TE_Computer_2019_Course_22.06.2021.pdf" target="_blank"><img src="./../applied-science/images/download.png" alt="Research Publications"></a>
+							<p>2016-17 Sem1</p>
+						</div>
+
+						<div class="col-md-3">
+							<a href="pdf/download21/project_list_2020-21.pdf" target="_blank"><img src="./../applied-science/images/download.png" alt="Research Publications"></a>
+							<p>2016-17 Sem2</p>
+						</div>
+
+						<div class="col-md-3">
+							<a href="pdf/download21/Syllabus%20TE%20Computer%20Engineering%202012Course.pdf" target="_blank"><img src="./../applied-science/images/download.png" alt="Research Publications"></a>
+							<p>2015-16 Sem1</p>
+						</div>
+
+						<div class="col-md-3">
+							<a href="pdf/download21/TE_Computer_2019_Course_22.06.2021.pdf" target="_blank"><img src="./../applied-science/images/download.png" alt="Research Publications"></a>
+							<p>2015-16 Sem2</p>
+						</div>
+
+
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<?php include '../footer.php'; ?>
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="../js/bootstrap.min.js"></script>
+	<script src="../js/menu.js"></script>
+
+</body>
+
+</html>
