68917cbc6a9f91d7ed07660ed8d0a9a581e5ae79
Rajat Hemant Deore <rajat.72002@gmail.com>
2022-10-31 09:30:49 +0530

    Day 7

---
+++ computer/questions.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Download - Previous Year Question Papers of Department of Computer Engineering | Pimpri Chinchwad College of Engineering And Research | PCCOER - PCMC, Pune</title>
+    <meta name="description" content="Download the previous year question papers of department of computer engineering at Pimpri Chinchwad College of Engineering And Research." />
+    <meta name="keywords" content="pimpri chinchwad college of engineering and research, computer engineering, previous year question paper">
+
+    <!-- Bootstrap -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+    <link href="../css/pccoer.css" rel="stylesheet">
+    <link href="../css/menu.css" rel="stylesheet">
+    <link rel="icon" href="../images/pccoer-favicon-icon.png" type="image/png">
+
+    <!-- my style -->
+    <link href="./../computer/computer.css" rel="stylesheet">
+
+    <!--Font Awesome Stylesheets -->
+    <link href="../css/all.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+</head>
+
+<body>
+
+    <?php include '../header.php'; ?>
+
+    <div class="inner-page-banner"></div>
+
+    <div class="page_breadcrumb">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><i class="fa fa-home"></i> Home</li>
+                            <li class="breadcrumb-item">Computer</li>
+                            <li class="breadcrumb-item"><a href="#">Quetion Papers</a></li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="page_details">
+        <div class="container">
+            <div class="row">
+
+                <?php include 'computer-sidebar.php'; ?>
+
+                <div class="col-xs-12 col-sm-12 col-md-9">
+                    <div class="hdg-bdr"><span class="hdg-bdr-1"></span><span class="hdg-bdr-2"></span><span class="hdg-bdr-3"></span></div>
+                    <br>
+                    <h1 align="center">Computer Question Papers</h1>
+                    <hr>
+                    <ul class="research">
+                        <li>
+                            <div class="research_main">
+                                <div>
+                                    <a href="http://pccoerelearning.com">
+                                        <p class="research_text">BE Question Paper</p>
+                                    </a>
+                                </div>
+                                <div>
+                                    <a href="pdf/Faculty_publication_YearWise_Details.pdf" class="elibrary_link"><i class="fa-solid fa-link"></i></a>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="research_main">
+                                <div>
+                                    <a href="http://www.unipune.ac.in/university_files/old_papers.htm">
+                                        <p class="research_text">TE Question Paper</p>
+                                    </a>
+                                </div>
+                                <div>
+                                    <a href="http://www.unipune.ac.in/university_files/old_papers.htm" class="elibrary_link"><i class="fa-solid fa-link"></i></a>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="research_main">
+                                <div>
+                                    <a href="https://www.coursera.org/">
+                                        <p class="research_text">SE Question Paper</p>
+                                    </a>
+                                </div>
+                                <div>
+                                    <a href="https://www.coursera.org/" class="elibrary_link"><i class="fa-solid fa-link"></i></a>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+
+
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+
+    <?php include '../footer.php'; ?>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/menu.js"></script>
+
+</body>
+
+</html>
