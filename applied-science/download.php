0f65f4bcb99ba2b1c7c116da7758740255e65a3e
Rajat Hemant Deore <rajat.72002@gmail.com>
2022-11-01 16:49:45 +0530

    Day 8

--- applied-science/download.php
+++ applied-science/download.php
@@ -1,39 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title> Download - First Year Engineering Time Table of Department of Applied Science | Pimpri Chinchwad College Of Engineering And Research | PCCOER - PCMC, Pune
-    </title>
-    <meta name="description" content="Download the first year engineering time table of department of applied science Pimpri Chinchwad College Of Engineering And Research.
-    "/>
-  <meta name="keywords" content="pimpri chinchwad college of engineering and research, applied science, time table">
-    <!-- Bootstrap -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
-    <link href="../css/style.css" rel="stylesheet">
-    <link href="../css/pccoer.css" rel="stylesheet">
-    <link href="../css/menu.css" rel="stylesheet">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<title> Download - First Year Engineering Time Table of Department of Applied Science | Pimpri Chinchwad College Of Engineering And Research | PCCOER - PCMC, Pune
+	</title>
+	<meta name="description" content="Download the first year engineering time table of department of applied science Pimpri Chinchwad College Of Engineering And Research.
+    " />
+	<meta name="keywords" content="pimpri chinchwad college of engineering and research, applied science, time table">
+	<!-- Bootstrap -->
+	<link href="../css/bootstrap.min.css" rel="stylesheet">
+	<link href="../css/style.css" rel="stylesheet">
+	<link href="../css/pccoer.css" rel="stylesheet">
+	<link href="../css/menu.css" rel="stylesheet">
 	<link rel="icon" href="../images/pccoer-favicon-icon.png" type="image/png">
-	
+
 	<!--Font Awesome Stylesheets -->
 	<link href="../css/all.min.css" rel="stylesheet">
-	
-  </head>
-  <body>
-  
-	<?php include '../header.php';?>
-	
+
+</head>
+
+<body>
+
+	<?php include '../header.php'; ?>
+
 	<div class="inner-page-banner"></div>
-	
+
 	<div class="page_breadcrumb">
 		<div class="container">
 			<div class="row">
 				<div class="col-md-12">
 					<nav aria-label="breadcrumb">
 						<ol class="breadcrumb">
-							<li class="breadcrumb-item"><i class="fa fa-home"></i> Home</li>					
+							<li class="breadcrumb-item"><i class="fa fa-home"></i> Home</li>
 							<li class="breadcrumb-item">Applied Science</li>
 							<li class="breadcrumb-item"><a href="#">Download</a></li>
 						</ol>
@@ -42,107 +44,127 @@
 			</div>
 		</div>
 	</div>
-		
+
 	<div class="page_details">
 		<div class="container">
 			<div class="row">
-				
-				<?php include 'applied-science-sidebar.php';?>
-				
+
+				<?php include 'applied-science-sidebar.php'; ?>
+
 				<div class="col-xs-12 col-sm-12 col-md-9">
 					<div class="hdg-bdr"><span class="hdg-bdr-1"></span><span class="hdg-bdr-2"></span><span class="hdg-bdr-3"></span></div>
 					<br>
 					<h1 align="center">Download</h1>
-					<hr> 
-						
+					<hr>
+
 					<div class="col-md-12">
-							
-							<div class="col-md-3">
-								<a href="pdf/2020-21_1_term%201.pdf" target="_blank"><img src="images/download.png"  alt="Research Publications"></a>
-								<p>Master TT 2020-21 Term-I</p>
-                            </div>
-						
-							<div class="col-md-3">
-								<a href="pdf/2020-21_2_term%202.pdf" target="_blank"><img src="images/download.png"  alt="Research Publications"></a>
-								<p>Master TT 2020-21 Term-II</p>
-                            </div>
-							
-							<div class="col-md-3">
-								<a href="pdf/Master%20TT%202019-20_%20Term-I.pdf" target="_blank"><img src="images/download.png"  alt="Research Publications"></a>
-								<p>Master TT 2019-20 Term-I</p>
-                            </div>
-							
-							<div class="col-md-3">
-								<a href="pdf/Master%20TT%202019-20_%20Term-II.pdf" target="_blank"><img src="images/download.png"  alt="Research Publications"></a>
-								<p>Master TT 2019-20 Term-II</p>
-                            </div>
-							
-							<div class="col-md-3">
-								<a href="pdf/First-Year-Engineering-2019-pattern-syllabus.pdf" target="_blank"><img src="images/download.png"  alt="Research Publications"></a>
-								<p>First Year Engineering 2019 pattern syllabus</p>
-                            </div>
-							<div class="col-md-3">
-								<a href="pdf/fe-credit-system-syllabus%202015%20pattern.pdf" target="_blank"><img src="images/download.png"  alt="Research Publications"></a>
-								<p>Fe Credit System Syllabus 2015 pattern</p>
-                            </div>
-							
-							<div class="col-md-3">
-								<a href="pdf/1acedemic%20calendar.pdf" target="_blank"><img src="images/download.png"  alt="Research Publications"></a>
-								<p>Acedemic Calender 2020-21</p>
-                            </div>
-							
-							<div class="col-md-3">
-								<a href="pdf/Academic_Calendar.PDF" target="_blank"><img src="images/download.png"  alt="Research Publications"></a>
-								<p>Academic Calendar 2019-20</p>
-                            </div>
-							<div class="col-md-3">
-								<a href="pdf/FE%20EVENT%20CALENDAR%2020-21%20TERM%201.pdf" target="_blank"><img src="images/download.png"  alt="Research Publications"></a>
-								<p>FE EVENT CALENDAR 20-21 TERM 1</p>
-                            </div>
-							
-							<div class="col-md-3">
-								<a href="pdf/FE%20EVENT%20CALENDAR%2019-20%20TERM%201.pdf" target="_blank"><img src="images/download.png"  alt="Research Publications"></a>
-								<p>FE EVENT CALENDAR 19-20 TERM 1</p>
-                            </div>
-							
-							<div class="col-md-3">
-								<a href="pdf/FE%20EVENT%20CALENDAR%2019-20%20TERM%202.pdf" target="_blank"><img src="images/download.png"  alt="Research Publications"></a>
-								<p>FE EVENT CALENDAR 19-20 TERM 2</p>
-                            </div>
-							<div class="col-md-3">
-								<a href="pdf/FE%20EVENT%20CALENDAR%2018-19%20TERM%202.pdf" target="_blank"><img src="images/download.png"  alt="Research Publications"></a>
-								<p>FE EVENT CALENDAR 18-19 TERM 2</p>
-                            </div>
-							
-							<div class="col-md-3">
-								<a href="pdf/Scanned%20Copy%20of%20FE%20Event%20Calendar%20Term%202%202020-21.pdf" target="_blank"><img src="images/download.png"  alt="Research Publications"></a>
-								<p>Scanned Copy of FE Event Calendar Term 2 2020-21</p>
-                            </div>
-							<div class="col-md-3">
-								<a href="pdf/First-Year-Engineering-2019-pattern-syllabus.pdf" target="_blank"><img src="images/download.png"  alt="Research Publications"></a>
-								<p>First Year Engineering 2019 Pattern Syllabus</p>
-                            </div>
-							
-							<div class="col-md-3">
-								<a href="pdf/TT_Prelim.pdf" target="_blank"><img src="images/download.png"  alt="Research Publications"></a>
-								<p>Time Table Prelim</p>
-                            </div>
-							<div class="col-md-3">
-								<a href="pdf/Scanned%20Copy%20of%20FE%20Event%20Calendar%20Term%202%202020-21%20(1).pdf" target="_blank"><img src="images/download.png"  alt="Research Publications"></a>
-								<p>FE Event Calendar</p>
-                            </div>
-							 
-                    </div>
+
+						<div class="col-md-3">
+							<a href="pdf/Master Time Table 21-22 Sem -I.pdf" target="_blank"><img src="images/download.png" alt="Research Publications"></a>
+							<p>Master TT 2021-22 Term-I</p>
+						</div>
+
+						<div class="col-md-3">
+							<a href="pdf/Master Time Table 21-22 Sem-II.pdf" target="_blank"><img src="images/download.png" alt="Research Publications"></a>
+							<p>Master TT 2021-22 Term-II</p>
+						</div>
+
+						<div class="col-md-3">
+							<a href="pdf/Event Calendar Term 1 2021-22.pdf" target="_blank"><img src="images/download.png" alt="Research Publications"></a>
+							<p>FE EVENT CALENDAR 21-22 TERM 1</p>
+						</div>
+
+						<div class="col-md-3">
+							<a href="pdf/Event Calender 2021-22 Term II.pdf" target="_blank"><img src="images/download.png" alt="Research Publications"></a>
+							<p>FE EVENT CALENDAR 21-22 TERM 2</p>
+						</div>
+						<div class="col-md-3">
+							<a href="pdf/2020-21_1_term%201.pdf" target="_blank"><img src="images/download.png" alt="Research Publications"></a>
+							<p>Master TT 2020-21 Term-I</p>
+						</div>
+
+						<div class="col-md-3">
+							<a href="pdf/2020-21_2_term%202.pdf" target="_blank"><img src="images/download.png" alt="Research Publications"></a>
+							<p>Master TT 2020-21 Term-II</p>
+						</div>
+
+						<div class="col-md-3">
+							<a href="pdf/Master%20TT%202019-20_%20Term-I.pdf" target="_blank"><img src="images/download.png" alt="Research Publications"></a>
+							<p>Master TT 2019-20 Term-I</p>
+						</div>
+
+						<div class="col-md-3">
+							<a href="pdf/Master%20TT%202019-20_%20Term-II.pdf" target="_blank"><img src="images/download.png" alt="Research Publications"></a>
+							<p>Master TT 2019-20 Term-II</p>
+						</div>
+
+						<div class="col-md-3">
+							<a href="pdf/First-Year-Engineering-2019-pattern-syllabus.pdf" target="_blank"><img src="images/download.png" alt="Research Publications"></a>
+							<p>First Year Engineering 2019 pattern syllabus</p>
+						</div>
+						<div class="col-md-3">
+							<a href="pdf/fe-credit-system-syllabus%202015%20pattern.pdf" target="_blank"><img src="images/download.png" alt="Research Publications"></a>
+							<p>Fe Credit System Syllabus 2015 pattern</p>
+						</div>
+
+						<div class="col-md-3">
+							<a href="pdf/1acedemic%20calendar.pdf" target="_blank"><img src="images/download.png" alt="Research Publications"></a>
+							<p>Acedemic Calender 2020-21</p>
+						</div>
+
+						<div class="col-md-3">
+							<a href="pdf/Academic_Calendar.PDF" target="_blank"><img src="images/download.png" alt="Research Publications"></a>
+							<p>Academic Calendar 2019-20</p>
+						</div>
+						<div class="col-md-3">
+							<a href="pdf/FE%20EVENT%20CALENDAR%2020-21%20TERM%201.pdf" target="_blank"><img src="images/download.png" alt="Research Publications"></a>
+							<p>FE EVENT CALENDAR 20-21 TERM 1</p>
+						</div>
+
+						<div class="col-md-3">
+							<a href="pdf/FE%20EVENT%20CALENDAR%2019-20%20TERM%201.pdf" target="_blank"><img src="images/download.png" alt="Research Publications"></a>
+							<p>FE EVENT CALENDAR 19-20 TERM 1</p>
+						</div>
+
+						<div class="col-md-3">
+							<a href="pdf/FE%20EVENT%20CALENDAR%2019-20%20TERM%202.pdf" target="_blank"><img src="images/download.png" alt="Research Publications"></a>
+							<p>FE EVENT CALENDAR 19-20 TERM 2</p>
+						</div>
+						<div class="col-md-3">
+							<a href="pdf/FE%20EVENT%20CALENDAR%2018-19%20TERM%202.pdf" target="_blank"><img src="images/download.png" alt="Research Publications"></a>
+							<p>FE EVENT CALENDAR 18-19 TERM 2</p>
+						</div>
+
+						<div class="col-md-3">
+							<a href="pdf/Scanned%20Copy%20of%20FE%20Event%20Calendar%20Term%202%202020-21.pdf" target="_blank"><img src="images/download.png" alt="Research Publications"></a>
+							<p>Scanned Copy of FE Event Calendar Term 2 2020-21</p>
+						</div>
+						<div class="col-md-3">
+							<a href="pdf/First-Year-Engineering-2019-pattern-syllabus.pdf" target="_blank"><img src="images/download.png" alt="Research Publications"></a>
+							<p>First Year Engineering 2019 Pattern Syllabus</p>
+						</div>
+
+						<div class="col-md-3">
+							<a href="pdf/TT_Prelim.pdf" target="_blank"><img src="images/download.png" alt="Research Publications"></a>
+							<p>Time Table Prelim</p>
+						</div>
+						<div class="col-md-3">
+							<a href="pdf/Scanned%20Copy%20of%20FE%20Event%20Calendar%20Term%202%202020-21%20(1).pdf" target="_blank"><img src="images/download.png" alt="Research Publications"></a>
+							<p>FE Event Calendar</p>
+						</div>
+
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-	
-	<?php include '../footer.php';?>
+
+	<?php include '../footer.php'; ?>
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
-    <script src="../js/menu.js"></script>
-	
-  </body>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="../js/bootstrap.min.js"></script>
+	<script src="../js/menu.js"></script>
+
+</body>
+
 </html>
